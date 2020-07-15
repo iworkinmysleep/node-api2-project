157c59650546234e8aa87cc46f89381f9560d042
Jason Hinton <jason_hinton@att.net>
2020-07-15 00:55:14 -0400

    post endpoints

--- post-router.js
+++ post-router.js
@@ -1,15 +1,53 @@
-const express = require('express');
+const express = require("express");
 
-const Db = require('./data/db.js');
+const Posts = require("./data/db.js");
+const { json } = require("express");
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
-  
-})
+// Post
+router.post("/", (req, res) => {
+	const { title, contents } = req.body;
+	if (!title || !contents) {
+		res.status(400).json({
+			errorMessage: "Please provide title and contents for the post.",
+		});
+	}
+	Posts.insert({ title, contents })
+		.then((post) => {
+			res.status(201).json(post);
+		})
+		.catch((err) => {
+			res.status(500).json({
+				error: "There was an error while saving the post to the database",
+			});
+		});
+});
 
-
-
-
+//Post comment
+router.post("/:id/comments", (req, res) => {
+	const postId = req.params.id;
+	const { text } = req.body;
+	Posts.findById(postId)
+		.then((post) => {
+			if (!post) {
+				res
+					.status(404)
+					.json({ message: "The post with the specified ID does not exist." });
+			} else if (!text) {
+				res
+					.status(400)
+					.json({ errorMessage: "Please provide text for the comment." });
+			} else {
+				const comment = Posts.insertComment(text);
+				res.status(201).json(comment);
+			}
+		})
+		.catch((err) => {
+			res.status(500).json({
+				error: "There was an error while saving the comment to the database",
+			});
+		});
+});
 
 module.exports = router;
