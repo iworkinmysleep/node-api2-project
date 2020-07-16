3591b955608b7cd38e5216ce2c4f322961e770ac
Jason Hinton <jason_hinton@att.net>
2020-07-16 00:59:53 -0400

    all but update

--- post-router.js
+++ post-router.js
@@ -27,27 +27,104 @@ router.post("/", (req, res) => {
 //Post comment
 router.post("/:id/comments", (req, res) => {
 	const postId = req.params.id;
-	const { text } = req.body;
-	Posts.findById(postId)
+	const newComment = req.body;
+	newComment.post_id = Number(postId);
+	if (!newComment.text) {
+		res.status(400).json({ error: "Please provide text field." });
+	} else {
+		Posts.findPostComments(postId)
+			.then((comment) => {
+				if (comment) {
+					Posts.insertComment(newComment);
+					res.status(201).json(newComment);
+				} else {
+					res
+						.status(404)
+						.json({ error: "The post with the specified ID does not exist." });
+				}
+			})
+			.catch((err) => {
+				res.status(500).json({
+					error: "There was an error while saving the comment to the database.",
+					err,
+				});
+			});
+	}
+});
+
+// Get
+router.get("/", (req, res) => {
+	Posts.find(req.query)
+		.then((posts) => {
+			res.status(200).json(posts);
+		})
+		.catch((err) => {
+			res
+				.status(500)
+				.json({ error: "There was an error retrieving posts", err });
+		});
+});
+
+// Get by Id
+router.get("/:id", (req, res) => {
+	Posts.findById(req.params.id)
 		.then((post) => {
-			if (!post) {
+			if (post) {
+				res.status(200).json(post);
+			} else {
 				res
 					.status(404)
-					.json({ message: "The post with the specified ID does not exist." });
-			} else if (!text) {
-				res
-					.status(400)
-					.json({ errorMessage: "Please provide text for the comment." });
-			} else {
-				const comment = Posts.insertComment(text);
-				res.status(201).json(comment);
+					.json({ error: "The post with the specified ID does not exist" });
 			}
 		})
 		.catch((err) => {
-			res.status(500).json({
-				error: "There was an error while saving the comment to the database",
-			});
+			res
+				.status(500)
+				.json({ error: "There was an error connecting to the database.", err });
 		});
 });
+
+// Get comment by ID
+router.get("/:id/comments", (req, res) => {
+	Posts.findCommentById(req.params.id)
+		.then((comment) => {
+			if (comment) {
+				res.status(200).json(comment);
+			} else {
+				res
+					.status(404)
+					.json({ message: "The post with the specified ID does not exist." });
+			}
+		})
+		.catch((err) => {
+			res
+				.status(500)
+				.json({
+					error: "The comments information could not be retrieved.",
+					err,
+				});
+		});
+});
+
+// Delete
+router.delete("/:id", (req, res) => {
+	Posts.remove(req.params.id)
+		.then((post) => {
+			if (post) {
+				res.status(200).json(post);
+			} else {
+				res
+					.status(404)
+					.json({ message: "The post with the specified ID does not exist." });
+			}
+		})
+		.catch((err) => {
+			res.status(500).json({ error: "The post could not be removed", err });
+		});
+});
+
+//Update
+
+
 
 module.exports = router;
